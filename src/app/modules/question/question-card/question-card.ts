9d854cf88a2294bcc2ff4aaf443c340b74a5d171
paulelyson <paulelyson.villaceran@gmail.com>
2026-03-27 23:50:27 +0800

    feat: enhance QuestionCard component with choice selection and styling

--- src/app/modules/question/question-card/question-card.ts
+++ src/app/modules/question/question-card/question-card.ts
@@ -4,11 +4,17 @@ import { Component, Input } from '@angular/core';
   selector: 'app-question-card',
   templateUrl: './question-card.html',
   styleUrl: './question-card.scss',
-  standalone: false
+  standalone: false,
 })
 export class QuestionCard {
   @Input() question: string = '';
   @Input() answer: string = '';
   @Input() showAnswer: boolean = false;
   @Input() choices: string[] = [];
+  selectedChoice: string | null = null;
+
+  selectChoice(choice: string) {
+    this.selectedChoice = choice;
+    this.showAnswer = true;
+  }
 }
